191ddc7b254818d95a11fadc5551bcd0bcf86313
victorletichevsky <victorletichevsky@gmail.com>
2022-09-03 14:03:46 -0300

    Initial Commit

---
+++ Petro/PetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetroApp.swift
+//  Petro
+//
+//  Created by Victor Letichevsky on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
